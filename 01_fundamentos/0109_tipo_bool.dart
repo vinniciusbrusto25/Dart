21dcb614e2f259a8424ab7e812c20b08566d06d1
Vinicius Brustolin <vinnicius_brusto@hotmail.com>
2022-07-03 19:26:33 -0300

    booleanos

---
+++ 01_fundamentos/0109_tipo_bool.dart
@@ -0,0 +1,11 @@
+//mesma sintaxe do C.
+
+void main() {
+  bool estaChovendo = true;
+  bool muitoFrio = false;
+
+  print(estaChovendo.runtimeType);
+  print(muitoFrio);
+  print(muitoFrio.toString());
+  print(muitoFrio.toString().toUpperCase());
+}
